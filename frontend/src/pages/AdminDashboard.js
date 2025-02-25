b301544b347f846534fd0597dca030c5f54b1af6
Akshada Patil <akshadapatil009@gmail.com>
2025-02-25 11:24:47 +0530

    admin dash

---
+++ frontend/src/pages/AdminDashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <Header role="admin" />
+      <h2>Admin Dashboard</h2>
+      <Footer />
+    </div>
+  );
+};
+
+export default AdminDashboard;
