43341d2421b89cfa99f6a6e54ef37011b34fd62d
Akshada Patil <akshadapatil009@gmail.com>
2025-02-25 11:20:04 +0530

    footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer style={{ background: "#333", color: "#fff", textAlign: "center", padding: "10px", marginTop: "20px" }}>
+      <p>© 2025 Attendance App</p>
+    </footer>
+  );
+};
+
+export default Footer;
